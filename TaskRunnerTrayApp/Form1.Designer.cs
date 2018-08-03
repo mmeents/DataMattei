56473de961fa4849a625a1b69295e2dbbbd237ef
Matt Meents <matt27r@yahoo.com>
2018-08-03 01:49:47 -0400

    TaskRunner adds scheduler

--- TaskRunnerTrayApp/Form1.Designer.cs
+++ TaskRunnerTrayApp/Form1.Designer.cs
@@ -29,7 +29,7 @@
       this.FD = new System.Windows.Forms.FolderBrowserDialog();
       this.tabControl1 = new System.Windows.Forms.TabControl();
       this.tabPage1 = new System.Windows.Forms.TabPage();
-      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.lbOutlook = new System.Windows.Forms.ListBox();
       this.lbRow1 = new System.Windows.Forms.Label();
       this.lbRow0 = new System.Windows.Forms.Label();
       this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -40,6 +40,7 @@
       this.editTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.removeTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.tAppClock = new System.Windows.Forms.Timer(this.components);
+      this.lbTime2 = new System.Windows.Forms.Label();
       this.tabControl1.SuspendLayout();
       this.tabPage1.SuspendLayout();
       this.tabPage2.SuspendLayout();
@@ -56,59 +57,67 @@
       this.tabControl1.Controls.Add(this.tabPage2);
       this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.tabControl1.Location = new System.Drawing.Point(0, 0);
+      this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.tabControl1.Name = "tabControl1";
       this.tabControl1.SelectedIndex = 0;
-      this.tabControl1.Size = new System.Drawing.Size(511, 460);
+      this.tabControl1.Size = new System.Drawing.Size(766, 708);
       this.tabControl1.TabIndex = 0;
       // 
       // tabPage1
       // 
-      this.tabPage1.Controls.Add(this.listBox1);
+      this.tabPage1.Controls.Add(this.lbOutlook);
       this.tabPage1.Controls.Add(this.lbRow1);
       this.tabPage1.Controls.Add(this.lbRow0);
-      this.tabPage1.Location = new System.Drawing.Point(4, 22);
+      this.tabPage1.Location = new System.Drawing.Point(4, 29);
+      this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.tabPage1.Name = "tabPage1";
-      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage1.Size = new System.Drawing.Size(503, 434);
+      this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.tabPage1.Size = new System.Drawing.Size(758, 675);
       this.tabPage1.TabIndex = 0;
       this.tabPage1.Text = "Schedule";
       this.tabPage1.UseVisualStyleBackColor = true;
       // 
-      // listBox1
+      // lbOutlook
       // 
-      this.listBox1.FormattingEnabled = true;
-      this.listBox1.Location = new System.Drawing.Point(27, 78);
-      this.listBox1.Name = "listBox1";
-      this.listBox1.Size = new System.Drawing.Size(436, 121);
-      this.listBox1.TabIndex = 2;
-      this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+      this.lbOutlook.FormattingEnabled = true;
+      this.lbOutlook.ItemHeight = 20;
+      this.lbOutlook.Location = new System.Drawing.Point(40, 120);
+      this.lbOutlook.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.lbOutlook.Name = "lbOutlook";
+      this.lbOutlook.Size = new System.Drawing.Size(652, 184);
+      this.lbOutlook.TabIndex = 2;
+      this.lbOutlook.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
       // 
       // lbRow1
       // 
       this.lbRow1.AutoSize = true;
-      this.lbRow1.Location = new System.Drawing.Point(8, 34);
+      this.lbRow1.Location = new System.Drawing.Point(12, 52);
+      this.lbRow1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.lbRow1.Name = "lbRow1";
-      this.lbRow1.Size = new System.Drawing.Size(43, 13);
+      this.lbRow1.Size = new System.Drawing.Size(62, 20);
       this.lbRow1.TabIndex = 1;
       this.lbRow1.Text = "lbRow1";
       // 
       // lbRow0
       // 
       this.lbRow0.AutoSize = true;
-      this.lbRow0.Location = new System.Drawing.Point(8, 9);
+      this.lbRow0.Location = new System.Drawing.Point(12, 14);
+      this.lbRow0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.lbRow0.Name = "lbRow0";
-      this.lbRow0.Size = new System.Drawing.Size(35, 13);
+      this.lbRow0.Size = new System.Drawing.Size(51, 20);
       this.lbRow0.TabIndex = 0;
       this.lbRow0.Text = "label1";
       // 
       // tabPage2
       // 
+      this.tabPage2.Controls.Add(this.lbTime2);
       this.tabPage2.Controls.Add(this.label2);
       this.tabPage2.Controls.Add(this.lbTasks);
-      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Location = new System.Drawing.Point(4, 29);
+      this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.tabPage2.Name = "tabPage2";
-      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage2.Size = new System.Drawing.Size(503, 434);
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.tabPage2.Size = new System.Drawing.Size(758, 675);
       this.tabPage2.TabIndex = 1;
       this.tabPage2.Text = "Additional Details";
       this.tabPage2.UseVisualStyleBackColor = true;
@@ -116,9 +125,10 @@
       // label2
       // 
       this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(28, 23);
+      this.label2.Location = new System.Drawing.Point(42, 35);
+      this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(39, 13);
+      this.label2.Size = new System.Drawing.Size(55, 20);
       this.label2.TabIndex = 1;
       this.label2.Text = "Tasks ";
       // 
@@ -126,38 +136,41 @@
       // 
       this.lbTasks.ContextMenuStrip = this.contextMenuStrip1;
       this.lbTasks.FormattingEnabled = true;
-      this.lbTasks.Location = new System.Drawing.Point(45, 52);
+      this.lbTasks.ItemHeight = 20;
+      this.lbTasks.Location = new System.Drawing.Point(68, 80);
+      this.lbTasks.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.lbTasks.Name = "lbTasks";
-      this.lbTasks.Size = new System.Drawing.Size(387, 108);
+      this.lbTasks.Size = new System.Drawing.Size(578, 164);
       this.lbTasks.TabIndex = 0;
       // 
       // contextMenuStrip1
       // 
+      this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
       this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addTaskToolStripMenuItem,
             this.editTaskToolStripMenuItem,
             this.removeTaskToolStripMenuItem});
       this.contextMenuStrip1.Name = "contextMenuStrip1";
-      this.contextMenuStrip1.Size = new System.Drawing.Size(144, 70);
+      this.contextMenuStrip1.Size = new System.Drawing.Size(190, 94);
       // 
       // addTaskToolStripMenuItem
       // 
       this.addTaskToolStripMenuItem.Name = "addTaskToolStripMenuItem";
-      this.addTaskToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+      this.addTaskToolStripMenuItem.Size = new System.Drawing.Size(189, 30);
       this.addTaskToolStripMenuItem.Text = "Add Task";
       this.addTaskToolStripMenuItem.Click += new System.EventHandler(this.addTaskToolStripMenuItem_Click);
       // 
       // editTaskToolStripMenuItem
       // 
       this.editTaskToolStripMenuItem.Name = "editTaskToolStripMenuItem";
-      this.editTaskToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+      this.editTaskToolStripMenuItem.Size = new System.Drawing.Size(189, 30);
       this.editTaskToolStripMenuItem.Text = "Edit Task";
       this.editTaskToolStripMenuItem.Click += new System.EventHandler(this.editTaskToolStripMenuItem_Click);
       // 
       // removeTaskToolStripMenuItem
       // 
       this.removeTaskToolStripMenuItem.Name = "removeTaskToolStripMenuItem";
-      this.removeTaskToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+      this.removeTaskToolStripMenuItem.Size = new System.Drawing.Size(189, 30);
       this.removeTaskToolStripMenuItem.Text = "Remove Task";
       this.removeTaskToolStripMenuItem.Click += new System.EventHandler(this.removeTaskToolStripMenuItem_Click);
       // 
@@ -166,12 +179,23 @@
       this.tAppClock.Interval = 1000;
       this.tAppClock.Tick += new System.EventHandler(this.tAppClock_Tick);
       // 
+      // lbTime2
+      // 
+      this.lbTime2.AutoSize = true;
+      this.lbTime2.Location = new System.Drawing.Point(9, 5);
+      this.lbTime2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbTime2.Name = "lbTime2";
+      this.lbTime2.Size = new System.Drawing.Size(51, 20);
+      this.lbTime2.TabIndex = 2;
+      this.lbTime2.Text = "label1";
+      // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(511, 460);
+      this.ClientSize = new System.Drawing.Size(766, 708);
       this.Controls.Add(this.tabControl1);
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.Name = "Form1";
       this.Text = "Mattei Task Runner Properties";
       this.Load += new System.EventHandler(this.Form1_Load);
@@ -199,7 +223,8 @@
     private System.Windows.Forms.ToolStripMenuItem addTaskToolStripMenuItem;
     private System.Windows.Forms.ToolStripMenuItem editTaskToolStripMenuItem;
     private System.Windows.Forms.ToolStripMenuItem removeTaskToolStripMenuItem;
-    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.ListBox lbOutlook;
+    private System.Windows.Forms.Label lbTime2;
   }
 }
 
--- TaskRunnerTrayApp/Form1.cs
+++ TaskRunnerTrayApp/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using C0DEC0RE;
 using System.IO;
+using System.Diagnostics;
 
 namespace TrayApp1
 {
@@ -59,28 +60,74 @@ namespace TrayApp1
     private void checkBox1_CheckedChanged(object sender, EventArgs e) {}
     
     private void tAppClock_Tick(object sender, EventArgs e) {
-      DateTime aNow = DateTime.Now;
-      // show the now. 
-      lbRow0.Text = aNow.toStrDateTime();
+      tAppClock.Enabled = false;
+      try { 
+        DateTime aNow = DateTime.Now;
+        // show the now. 
+        lbRow0.Text = aNow.toStrDateTime();
+        lbTime2.Text = aNow.toStrDateTime();
 
-      if (iTaskCount <=0){ 
-        if (editTaskToolStripMenuItem.Enabled){
-          editTaskToolStripMenuItem.Enabled = false;
-          removeTaskToolStripMenuItem.Enabled = false;
+        
+        if (iTaskCount <=0){ 
+          if (editTaskToolStripMenuItem.Enabled){
+            editTaskToolStripMenuItem.Enabled = false;
+            removeTaskToolStripMenuItem.Enabled = false;
+          }
+        } else { 
+          if(!editTaskToolStripMenuItem.Enabled){ 
+            editTaskToolStripMenuItem.Enabled = true;
+            removeTaskToolStripMenuItem.Enabled = true;
+          }
         }
-      } else { 
-        if(!editTaskToolStripMenuItem.Enabled){ 
-          editTaskToolStripMenuItem.Enabled = true;
-          removeTaskToolStripMenuItem.Enabled = true;
+
+        IniFile ai = IniFile.FromFile(sFileSettings);
+        SortedList<DateTime, string> lSchedule = new SortedList<DateTime, string>();
+        iTaskCount = ai["Settings"]["TaskCount"].toInt32();
+        if (iTaskCount > 0){
+   
+          for (var i = 1; i <= iTaskCount; i++){
+            string sEnabled  = ai["Task" + i.ToString()]["TaskEnabled"];
+            Boolean TimeElapsed = false;
+            if (sEnabled=="true") {
+
+              DateTime aTime = ai["Task" + i.ToString()]["When"].toDateTime();
+            
+              if ((aTime != null)&&(aTime < aNow)){
+                aTime = AdvanceToWhen(aTime);
+                TimeElapsed = true;
+              }
+              string aName = ai["Task" + i.ToString()]["TaskName"];
+              string aTask = ai["Task" + i.ToString()]["WhatToDo"];
+              string aTaskParam = ai["Task" + i.ToString()]["WhatToDoParams"];
+
+              lSchedule.Add(aTime, i.ToString() + ": " + aName);
+
+              if (TimeElapsed){
+                ai["Task" + i.ToString()]["When"] = aTime.toStrDateTime();
+                ai.Save(sFileSettings);
+                LaunchCmd( aTask, aTaskParam);
+                Form1_Load(null, null);
+              }
+            }
+
+          
+          }
+          if (lSchedule.Count > 0) {
+            if (lbOutlook.Items.Count > 0) { lbOutlook.Items.Clear(); }
+            Boolean ftt = true;
+            foreach (DateTime d in lSchedule.Keys) {
+              lbOutlook.Items.Add(lSchedule[d] + "; " + d.toStrDateTime());
+              if (ftt) {
+                lbRow1.Text = "Next up: " + lSchedule[d]+" at "+ d.toStrDateTime();
+              }
+            }
+          }
+
         }
+      } finally {
+        tAppClock.Enabled = true;
       }
-
-
-      // get next scheduled task
-
-      // if anow > nextScheduledTask 
-      // then launch task.
-
+      
     }
 
     private void addTaskToolStripMenuItem_Click(object sender, EventArgs e) {
@@ -104,6 +151,49 @@ namespace TrayApp1
     private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
 
 
+    }
+
+    public DateTime AdvanceToWhen(DateTime aWhen)
+    {
+      DateTime aNow = DateTime.Now;
+      DateTime dWhen = aWhen;
+      if (aNow > dWhen)
+      {
+        while ((aNow > dWhen) && (dWhen.DayOfWeek != DayOfWeek.Sunday))
+        {
+          dWhen = dWhen.AddDays(1);
+        }
+      }
+
+      return dWhen;
+    }
+
+    public void LaunchCmd(string ExeName, string arguments)
+    {
+      try
+      {
+        ProcessStartInfo start = new ProcessStartInfo();
+        start.Arguments = arguments;  // Enter in the command line arguments, everything you would enter after the executable name itself
+                                      // Enter the executable to run, including the complete path
+        start.FileName = ExeName;
+        // Do you want to show a console window?
+        start.WindowStyle = ProcessWindowStyle.Normal;
+        start.CreateNoWindow = true;
+        //  int exitCode;
+
+        // Run the external process & wait for it to finish
+        using (Process proc = Process.Start(start))
+        {
+          //    proc.WaitForExit();
+          //    exitCode = proc.ExitCode;             // Retrieve the app's exit code
+          //    Application.DoEvents();
+        }
+
+      }
+      catch (Exception e)
+      {
+
+      }
     }
   }
 
