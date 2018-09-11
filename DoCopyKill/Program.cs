05c3790a29614a104f37f1bc2df2edc1e901176f
Mr Mattei <mmeents@blueskymls.com>
2018-09-11 16:26:37 -0400

    adds copy kill and auto sftp

---
+++ DoCopyKill/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Configuration;
+
+namespace DoCopyKill {
+
+  class Program {
+
+    static void Main(string[] args) {
+
+      string sPickupPath = ConfigurationManager.AppSettings["PickupPath"];
+      string sPickupMask = ConfigurationManager.AppSettings["PickupMask"];
+      string sDropOffPath = ConfigurationManager.AppSettings["DropOffPath"];
+      string sCopyTo = "";
+
+      List<string> lFiles = new List<string>();
+
+      AddFiles( sPickupPath, sPickupMask, lFiles);
+
+      foreach(string sFile in lFiles){ 
+        if(File.Exists(sFile)) {
+          try{
+            sCopyTo = ""; 
+            DateTime aFT = File.GetLastWriteTime(sFile);
+          
+            Int32 iMonth = aFT.Month;
+            string sQuarter = ( ((iMonth == 1)||(iMonth == 2)||(iMonth == 3))? "Q1" : 
+                                ( ((iMonth == 4)||(iMonth == 5)||(iMonth == 6))? "Q2" : 
+                                ( ((iMonth == 7)||(iMonth == 8)||(iMonth == 9))? "Q3" : 
+                                ( ((iMonth == 10)||(iMonth == 11)||(iMonth == 12))? "Q4": ""  ))));          
+            string sYear = aFT.Year.ToString();          
+            string sDestQu = sDropOffPath+"\\"+sYear+sQuarter;
+
+            if (!Directory.Exists(sDestQu)){ 
+              Directory.CreateDirectory(sDestQu);
+            }
+
+            sCopyTo = sDestQu +"\\"+ Path.GetFileName(sFile);          
+
+            //if (File.Exists(sCopyTo)){ 
+             // File.Delete(sCopyTo);
+            //}
+
+            if (!File.Exists(sCopyTo)){ 
+              File.Copy(sFile , sCopyTo );
+            }
+            
+          } catch (Exception ee) {
+            appUtils.LogException("DoCopy: From:\""+sFile+"\" To:"+sCopyTo ,ee);        
+          }
+        }
+      }
+
+
+       foreach(string sFile in lFiles){ 
+      //  File.Copy(sFile, sDropOffPath + "\\")
+        if(File.Exists(sFile)) {
+          try {
+            DateTime aFT = File.GetLastWriteTime(sFile);
+          
+            Int32 iMonth = aFT.Month;
+            string sQuarter = ( ((iMonth == 1)||(iMonth == 2)||(iMonth == 3))? "Q1" : 
+                                ( ((iMonth == 4)||(iMonth == 5)||(iMonth == 6))? "Q2" : 
+                                ( ((iMonth == 7)||(iMonth == 8)||(iMonth == 9))? "Q3" : 
+                                ( ((iMonth == 10)||(iMonth == 11)||(iMonth == 12))? "Q4": ""  ))));          
+            string sYear = aFT.Year.ToString();          
+            string sDestQu = sDropOffPath+"\\"+sYear+sQuarter+"\\";
+
+            if (!Directory.Exists(sDestQu)){ 
+              Directory.CreateDirectory(sDestQu);
+            }
+            sCopyTo = sDestQu +"\\"+ Path.GetFileName(sFile);                 
+
+            if(File.Exists(sCopyTo)) {
+              File.Delete(sFile);
+            }         
+          
+          } catch (Exception ee) {
+            appUtils.LogException("DoDelete: CheckFileMadeIt:"+sCopyTo+"+ OriginalToDelete:"+sFile,ee);        
+          }
+        }
+      }
+           
+
+      
+    }
+
+    static public void AddFiles( string sFilePath, string sMask, List<string> lFiles ){ 
+
+      string[] dirs = Directory.GetDirectories(sFilePath);
+      if (dirs.Length > 0) {
+        foreach (string subDir in dirs) {
+          AddFiles(subDir+"\\", sMask, lFiles);
+        }
+      }
+
+      string[] filelist = Directory.GetFiles(sFilePath, sMask);
+      try {
+        if (filelist.Length > 0) {
+          foreach (string sFN in filelist) {
+            lFiles.Add(sFN);
+          }
+        }
+      } catch (Exception ee) {
+        appUtils.LogException("SpanDir:", ee);        
+      }
+
+    }  
+  }  
+
+  public static class appUtils {
+    public static void LogException(String sExceptionTag, Exception e) {
+      try {
+        String sMessage = e.Message;
+        String sSource = e.Source;
+        String sStack = e.StackTrace;
+        ("*** Error(" + sExceptionTag + "): " + sMessage + "; Source: " + sSource + "; Stack: " + sStack).toLog("PoloLog");
+        if (e.InnerException != null) {
+          LogException(sExceptionTag + "A", e.InnerException);
+        }
+      } catch { }
+    }
+
+    static public string UserLogLocation() {
+
+      String sUserDataDir = ConfigurationManager.AppSettings["ProcessPath"];
+      if (!Directory.Exists(sUserDataDir)) {
+        Directory.CreateDirectory(sUserDataDir);
+      }
+      return sUserDataDir;
+    }
+
+    static public string LogFileName(string sLogName) {
+      return UserLogLocation() + sLogName + ".txt";
+    }
+
+    static public string toLog(this string sMsg, string sLogName) {
+      using (StreamWriter w = File.AppendText(LogFileName(sLogName))) { w.WriteLine(DateTime.Now.ToString() + ":" + sMsg); }
+      return sMsg;
+    }
+
+  }
+
+}
---
+++ SFTPDownload/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using Tamir.SharpSsh;
+using Tamir.SharpSsh.jsch;
+
+namespace ZipDB {
+  class Program {
+
+    //  string sPickupPath = ConfigurationManager.AppSettings["PickupPath"];
+    //  string sPickupMask = ConfigurationManager.AppSettings["PickupMask"];
+    //  string sSFTPServer = ConfigurationManager.AppSettings["SFTPServer"];
+    //  string sSFTPUser = ConfigurationManager.AppSettings["SFTPUser"];
+    //  string sSFTPPass = ConfigurationManager.AppSettings["SFTPPass"];
+    //  string sSFTPDir = ConfigurationManager.AppSettings["SFTPDir"];
+    //  string[] filelist = Directory.GetFiles(sPickupPath, sPickupMask);
+
+    static void Main(string[] args) {
+      try { 
+        string sSFTPServer = "SFTP.Pickit.COM";
+        string sSFTPUser = "username";
+        string sSFTPPass = "password";        
+        string sDest1 = "c:\\DestNew\\";
+        string sDest2 = "S:\\Dest\\";
+        string sTmpSrc = "";
+        string sTmpDest = "";
+
+        List<string> sFiles = new List<string>();
+        sFiles.Clear();
+        Sftp ss = new Sftp(sSFTPServer, sSFTPUser, sSFTPPass);
+
+        try {
+          ss.Connect(22);
+          System.Collections.ArrayList ar = ss.GetFileList("/Outbound/");
+          foreach (string sItem in ar ){
+            try { 
+              sTmpSrc = "/Outbound/" + sItem;
+              if ((sItem != ".")&& (sItem != "..")) {
+                
+                if (sItem.Contains("New.txt")) {
+                  sTmpDest = sDest1 + sItem;                  
+                } else {
+                  sTmpDest = sDest2 + sItem;                  
+                }
+
+                if (File.Exists(sTmpDest)) {
+                  sFiles.Add(sTmpSrc);
+                  Console.WriteLine("skipping: " + sTmpSrc);
+                  ("Skipped: " + sTmpSrc).toLog("DownLoadLog");
+                } else  {
+                  ss.Get(sTmpSrc, sTmpDest);                 
+                  Console.WriteLine("downloaded: "+sTmpDest );
+                  ("DownLoaded: " + sTmpDest).toLog("DownLoadLog");
+                  if (File.Exists(sTmpDest)){
+                    sFiles.Add(sTmpSrc);
+                  }
+                }
+
+              }             
+            } catch (Exception e) { 
+              appUtils.LogException("GetFileError", e);
+            }
+          }
+        
+          Int32 iErrorCount = 0;
+          var prop = ss.GetType().GetProperty("SftpChannel", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+          var methodInfo = prop.GetGetMethod(true);
+          var sftpChannel = methodInfo.Invoke(ss, null);           
+          foreach (string sItem in sFiles) {
+            try {            
+              ((ChannelSftp)sftpChannel).rm(sItem);
+              Console.WriteLine("Removing: "+sItem);
+              ("Removed: "+sItem).toLog("RemoveLog");
+            } catch (Exception e){
+              appUtils.LogException("DeleteFileError", e);
+              iErrorCount = iErrorCount + 1;
+              if (iErrorCount > 3) { 
+                throw new Exception("Too many errors.");
+              }
+            }
+          }
+
+      } finally {
+        ss.Close();
+      }
+      Console.WriteLine("Done. Closeing Connections Bye. " );
+      System.Threading.Thread.Sleep(3000);
+
+    } catch (Exception ee) {
+        appUtils.LogException("OutBlockError", ee);
+      }
+
+    }   // end of main 
+  }
+
+  public static class appUtils
+  {
+    public static void LogException(String sExceptionTag, Exception e)
+    {
+      try
+      {
+        String sMessage = e.Message;
+        String sSource = e.Source;
+        String sStack = e.StackTrace;
+        ("*** Error(" + sExceptionTag + "): " + sMessage + "; Source: " + sSource + "; Stack: " + sStack).toLog("PoloLog");
+        if (e.InnerException != null)
+        {
+          LogException(sExceptionTag + "A", e.InnerException);
+        }
+      }
+      catch { }
+    }
+
+    static public string UserLogLocation()
+    {
+
+      String sUserDataDir = ConfigurationManager.AppSettings["ProcessPath"];
+      if (!Directory.Exists(sUserDataDir))
+      {
+        Directory.CreateDirectory(sUserDataDir);
+      }
+      return sUserDataDir;
+    }
+
+    static public string LogFileName(string sLogName)
+    {
+      return UserLogLocation() + sLogName + ".txt";
+    }
+
+    static public string toLog(this string sMsg, string sLogName)
+    {
+      using (StreamWriter w = File.AppendText(LogFileName(sLogName))) { w.WriteLine(DateTime.Now.ToString() + ":" + sMsg); }
+      return sMsg;
+    }
+
+  }
+
+}
